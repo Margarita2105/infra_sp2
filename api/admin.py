d6689d9efc9d45e827366ac680826f761b5759e7
Margarita2105 <khokhlova2105@yandex.ru>
2020-10-16 18:39:52 +0300

    com1

---
+++ api/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from library.models import Genre, Title, Category, Review, Comment
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("pk", "name", "slug")
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("pk", "name", "slug")
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ("pk", "name", "year", "description", "rating", "category")
+    list_filter = ["year", "rating"]
+    search_fields = ("name",)
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("pk", "title", "text", "author", "score", "pub_date")
+    list_filter = ["author", "score"]
+    search_fields = ("title", "text")
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("pk", "review", "text", "author", "pub_date")
+    list_filter = ["author", "review"]
+    search_fields = ("text", )
